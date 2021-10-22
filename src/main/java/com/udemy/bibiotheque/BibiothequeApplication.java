dddf220fd8ab97c110ad663965acd7c51caa434e
Bertrand <bertrandpi.bp@gmail.com>
2021-10-22 11:11:17 +0200

    création de mon projet bibliotheque

---
+++ src/main/java/com/udemy/bibiotheque/BibiothequeApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.bibiotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/bibiotheque/BibiothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.bibiotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
